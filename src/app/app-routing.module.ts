e13b473119ba80a466ee16429ee1b9d1126229a0
sudhakarvita <sudhakar.vita99@gmail.com>
2023-12-15 15:03:49 +0530

    Resonance basic dashboard created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',redirectTo:'mainpage',pathMatch:'full'},
+  { path:'mainpage',component:MainpageComponent},
+  { path:'dashboard',component:DashboardComponent, children:[
+    {path:'profile',component:ProfileComponent},
+    { path:'login',component:LoginComponent},
+    { path:'home',component:HomeComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ProfileComponent,
+    HomeComponent,
+    MainpageComponent,
+    LoginComponent,
+    
+  
+    
   ],
   imports: [
     BrowserModule,
